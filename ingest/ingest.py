b915ab6df9c30bc479476c7139e2c598bf276808
Adam Blaida <ajblaida@gmail.com>
2020-01-19 22:27:31 -0600

    Create pyinstaller exe spec

---
+++ ingest/ingest.py
@@ -0,0 +1,5 @@
+import sys
+
+def ingest(arguments):
+	print("called with args222", arguments)
+	
--- main.py
+++ main.py
@@ -1,23 +1,6 @@
-from phue import Bridge
+from ingest.ingest import ingest
 
-b = Bridge('192.168.0.109')
-
-b.connect()
-
-b.get_api()
-light_names = b.get_light_objects('name')
-print(light_names)
-userin = input("value")
+userin = input("value\n")
 while userin != "quit":
-	brightness = int(userin)
-	if brightness < 0:
-		brightness = 0
-	elif brightness > 254:
-		brightness = 254
-	
-	isOn = True if brightness > 0 else False
-
-	b.set_group('Living Room', 'on', isOn)
-	if isOn:
-		b.set_group('Living Room', 'bri', brightness)
-	userin = input("value")
+	ingest(userin.split())
+	userin = input("value\n")
